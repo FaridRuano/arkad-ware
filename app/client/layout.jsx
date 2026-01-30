0c7798cbf4730749653691f9f56cda4846047eb1
FaridRuano <fruanocm2777@gmail.com>
2026-01-30 10:33:12 -0500

    Update Client Footer

--- app/client/layout.jsx
+++ app/client/layout.jsx
@@ -1,5 +1,6 @@
 import ClientHeader from "@public/components/client/ClientHeader";
 import { auth } from "@auth";
+import Footer from "@public/components/shared/Footer";
 
 export default async function ClientLayout({ children }) {
     const session = await auth();
@@ -7,9 +8,13 @@ export default async function ClientLayout({ children }) {
     const userName = session?.user?.name || "Usuario";
 
     return (
-        <div className="container client">
-            <ClientHeader userName={userName} />
-            {children}
-        </div>
+        <>
+            <div className="container client">
+                <ClientHeader userName={userName} />
+                {children}
+            </div>
+            <Footer />
+        </>
+
     );
 }
